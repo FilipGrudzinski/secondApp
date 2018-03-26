4464f7a57c756cac184d036aa9cb97809e6264f2
FilipGrudzinski <grudzinski.filip@gmail.com>
2018-03-26 21:49:29 +0200

    SsecondApp

--- SecondApp/ViewController.swift
+++ SecondApp/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func buttonClicked(_ sender: Any) {
+        
+        if let age = textField.text {
+            
+            label.text = "You are " + age + " years old!"
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
